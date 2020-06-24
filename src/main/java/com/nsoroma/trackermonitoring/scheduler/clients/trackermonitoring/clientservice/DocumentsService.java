d70977068c47f86e298d420bd368feb2ef2b6f09
Ave <banabas@hawkmanlabs.com>
2020-06-24 02:09:53 +0000

    added a column with header name gsm last update

--- src/main/java/com/nsoroma/trackermonitoring/scheduler/clients/trackermonitoring/clientservice/DocumentsService.java
+++ src/main/java/com/nsoroma/trackermonitoring/scheduler/clients/trackermonitoring/clientservice/DocumentsService.java
@@ -11,16 +11,16 @@ import java.util.Set;
 @Service
 public class DocumentsService {
 
-    private static String[] columns = {"Label", "Customer Name", "Customer Id", "Last Gps Update",
+    private static String[] columns = {"Label", "Customer Name", "Customer Id", "*Last Gsm Update", "Last Gps Update",
             "Tracker Id", "Imei No.", "Model", "Phone Number", "Connection Status",
             "Tariff End Date", "Last Gps Signal Level", "Last Gps Latitude",
             "Last Gps Longitude", "Last Battery Level", "Last Gsm Signal Level", "Gsm NetworkName"};
 
 
 
-    public Sheet generateExcellSheet(Set<TrackerState> trackerStates) throws IOException {
+    public Sheet generateExcelSheet(Set<TrackerState> trackerStates, String name) throws IOException {
             Workbook workbook = new XSSFWorkbook();
-            Sheet sheet = workbook.createSheet("Tracker States");
+            Sheet sheet = workbook.createSheet(name);
             Font headerFont = workbook.createFont();
             headerFont.setBold(true);
             headerFont.setFontHeightInPoints((short) 14);
@@ -41,19 +41,20 @@ public class DocumentsService {
                 row.createCell(0).setCellValue(trackerState.getLabel());
                 row.createCell(1).setCellValue(trackerState.getCustomerName());
                 row.createCell(2).setCellValue(trackerState.getCustomerId());
-                row.createCell(3).setCellValue(trackerState.getLastGpsUpdate());
-                row.createCell(4).setCellValue(trackerState.getTrackerId());
-                row.createCell(5).setCellValue(trackerState.getImei());
-                row.createCell(6).setCellValue(trackerState.getModel());
-                row.createCell(7).setCellValue(trackerState.getPhoneNumber());
-                row.createCell(8).setCellValue(trackerState.getConnectionStatus());
-                row.createCell(9).setCellValue(trackerState.getTariffEndDate());
-                row.createCell(10).setCellValue(trackerState.getLastGpsSignalLevel());
-                row.createCell(11).setCellValue(trackerState.getLastGpsLatitude());
-                row.createCell(12).setCellValue(trackerState.getLastGpsLongitude());
-                row.createCell(13).setCellValue(trackerState.getLastBatteryLevel());
-                row.createCell(14).setCellValue(trackerState.getGsmSignalLevel());
-                row.createCell(15).setCellValue(trackerState.getGsmNetworkName());
+                row.createCell(3).setCellValue(trackerState.getLastGsmUpdate());
+                row.createCell(4).setCellValue(trackerState.getLastGpsUpdate());
+                row.createCell(5).setCellValue(trackerState.getTrackerId());
+                row.createCell(6).setCellValue(trackerState.getImei());
+                row.createCell(7).setCellValue(trackerState.getModel());
+                row.createCell(8).setCellValue(trackerState.getPhoneNumber());
+                row.createCell(9).setCellValue(trackerState.getConnectionStatus());
+                row.createCell(10).setCellValue(trackerState.getTariffEndDate());
+                row.createCell(11).setCellValue(trackerState.getLastGpsSignalLevel());
+                row.createCell(12).setCellValue(trackerState.getLastGpsLatitude());
+                row.createCell(13).setCellValue(trackerState.getLastGpsLongitude());
+                row.createCell(14).setCellValue(trackerState.getLastBatteryLevel());
+                row.createCell(15).setCellValue(trackerState.getGsmSignalLevel());
+                row.createCell(16).setCellValue(trackerState.getGsmNetworkName());
             }
 
             for (int i = 0; i < columns.length; i++) {
